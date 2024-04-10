ae7cd8653528196304d095e19a015e38eb31204b
quzard <1191890118@qq.com>
2024-04-11 00:08:52 +0800

    fix

--- server_stdout.go
+++ server_stdout.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"os"
-	"path/filepath"
 	"strconv"
 	"strings"
 	"sync"
@@ -11,24 +10,11 @@ import (
 	"time"
 
 	"golang.org/x/time/rate"
-	"gopkg.in/natefinch/lumberjack.v2"
 )
-
-var once sync.Once
 
 var (
-	logger        *lumberjack.Logger
-	loggerMu      sync.Mutex
-	rateLimiter   *rate.Limiter
-	rateLimiterMu sync.Mutex
-)
-
-const (
-	defaultLogDir      = "/logs"
-	defaultMaxSize     = 500 // MB
-	defaultMaxBackups  = 20
-	defaultMaxAge      = 28    // Days
-	defaultWriteRateMB = 100   // MB/s
+	rateLimiter        *rate.Limiter
+	defaultWriteRateMB = 300   // MB/s
 	defaultWriteBurst  = 10000 // Burst size in bytes
 )
 
@@ -40,71 +26,35 @@ func getEnvAsInt(name string, defaultValue int) int {
 	return defaultValue
 }
 
-func initLogger(filename string) {
-	if logger == nil {
-		logDir := os.Getenv("LOG_DIR")
-		if logDir == "" {
-			logDir = defaultLogDir
-		}
-		if _, err := os.Stat(logDir); os.IsNotExist(err) {
-			os.Mkdir(logDir, 0755)
-		}
-		maxSize := getEnvAsInt("LOG_MAX_SIZE", defaultMaxSize)
-		maxBackups := getEnvAsInt("LOG_MAX_BACKUPS", defaultMaxBackups)
-		maxAge := getEnvAsInt("LOG_MAX_AGE", defaultMaxAge)
-
-		logger = &lumberjack.Logger{
-			Filename:   filepath.Join(logDir, filename),
-			MaxSize:    maxSize, // megabytes
-			MaxBackups: maxBackups,
-			MaxAge:     maxAge, //days
-			Compress:   false,  // disabled by default
-		}
-	}
-}
-
-func appendToFile(filename string, data []byte) {
-	if logger == nil {
-		once.Do(func() {
-			initLogger(filename)
-		})
-	}
-
-	rateLimiterMu.Lock()
-	r := rateLimiter.ReserveN(time.Now(), len(data))
-	rateLimiterMu.Unlock()
-
-	if !r.OK() {
-		return // Not allowed to write due to rate limiting
-	}
-	time.Sleep(r.Delay())
-
-	loggerMu.Lock()
-	logger.Write(data)
-	loggerMu.Unlock()
-}
-
 func echoHandler(log string, minute int) {
 	var wg sync.WaitGroup
 	var processWg sync.WaitGroup
-	timer := time.NewTimer(time.Duration(minute) * time.Minute)
 	quit := make(chan struct{})
+	if minute != -1 {
+		timer := time.NewTimer(time.Duration(minute) * time.Minute)
+		go func() {
+			<-timer.C
+			close(quit)
+		}()
+	}
 
-	go func() {
-		<-timer.C
-		close(quit)
-	}()
 	var stdoutLogCount int64 = 0
-	var fileLogCount int64 = 0
-
-	logChannel1 := make(chan string, 10000)
+	logChannel := make(chan string, 10000)
 	processWg.Add(1)
 	go func() {
 		defer processWg.Done()
 		shouldPrint := os.Getenv("SHOULD_PRINT")
 		cnt := 0
-		for log := range logChannel1 {
+		for log := range logChannel {
 			cnt++
+
+			r := rateLimiter.ReserveN(time.Now(), len(log))
+
+			if !r.OK() {
+				continue
+			}
+			time.Sleep(r.Delay())
+
 			if shouldPrint == "on" {
 				fmt.Println("cnt:", cnt, ",", log)
 				atomic.AddInt64(&stdoutLogCount, 1)
@@ -112,21 +62,6 @@ func echoHandler(log string, minute int) {
 		}
 	}()
 
-	logChannel2 := make(chan string, 10000)
-	processWg.Add(1)
-	go func() {
-		defer processWg.Done()
-		shouldAppendToFile := os.Getenv("SHOULD_APPEND_TO_FILE")
-
-		cnt := 0
-		for log := range logChannel2 {
-			cnt++
-			if shouldAppendToFile == "on" {
-				appendToFile("output.txt", []byte(fmt.Sprintf("cnt:%d,%s\n", cnt, log)))
-				atomic.AddInt64(&fileLogCount, 1)
-			}
-		}
-	}()
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
@@ -135,20 +70,17 @@ func echoHandler(log string, minute int) {
 			case <-quit:
 				return
 			default:
-				logChannel1 <- fmt.Sprintf("log:%s", log)
-				logChannel2 <- fmt.Sprintf("log:%s", log)
+				logChannel <- fmt.Sprintf("log:%s", log)
 			}
 		}
 	}()
 	wg.Wait()
 
-	close(logChannel1)
-	close(logChannel2)
+	close(logChannel)
 
 	processWg.Wait()
-	atomic.AddInt64(&stdoutLogCount, 2)
-	fmt.Println("Total stdoutLogCount:", atomic.LoadInt64(&stdoutLogCount))
-	fmt.Println("Total fileLogCount:", atomic.LoadInt64(&fileLogCount))
+	atomic.AddInt64(&stdoutLogCount, 1)
+	fmt.Println("cnt:Total stdoutLogCount:", atomic.LoadInt64(&stdoutLogCount))
 }
 
 func main() {
